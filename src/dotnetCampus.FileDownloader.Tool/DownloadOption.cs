45006294c5a7244d0a92882a54cc92cbd7f58704
lindexi <lindexi_gd@163.com>
2020-07-10 17:55:49 +0800

    修复测试

---
+++ src/dotnetCampus.FileDownloader.Tool/DownloadOption.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace dotnetCampus.FileDownloader.Tool
+{
+    public class DownloadOption
+    {
+        [Option('u', "Url", Required = true)]
+        public string Url { get; set; }
+
+        [Option('o', "output", Required = true)]
+        public string Output { get; set; }
+    }
+}
--- src/dotnetCampus.FileDownloader.Tool/Program.cs
+++ src/dotnetCampus.FileDownloader.Tool/Program.cs
@@ -122,13 +122,4 @@ namespace dotnetCampus.FileDownloader.Tool
             }
         }
     }
-
-    public class DownloadOption
-    {
-        [Option('u', "Url", Required = true)]
-        public string Url { get; set; }
-
-        [Option('o', "output", Required = true)]
-        public string Output { get; set; }
-    }
 }
