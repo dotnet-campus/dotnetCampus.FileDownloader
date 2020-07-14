4fa4da3e2990bbc110c2d6f5c6cbbfee18d99e3f
lindexi <lindexi_gd@163.com>
2020-07-14 10:38:16 +0800

    加上注释

--- src/dotnetCampus.FileDownloader/RandomFileWriter.cs
+++ src/dotnetCampus.FileDownloader/RandomFileWriter.cs
@@ -108,14 +108,23 @@ namespace dotnetCampus.FileDownloader
             }
 
             /// <summary>
-            /// 文件开始写入
+            /// 文件开始写入的点
             /// </summary>
             public long FileStartPoint { get; }
 
+            /// <summary>
+            /// 表示从 <see cref="Data"/> 的读取点
+            /// </summary>
             public int DataOffset { get; }
 
+            /// <summary>
+            /// 写入文件的数据
+            /// </summary>
             public byte[] Data { get; }
 
+            /// <summary>
+            /// 表示从 <see cref="Data"/> 的读取长度
+            /// </summary>
             public int DataLength { get; }
 
             public TaskCompletionSource<bool> TaskCompletionSource { get; }
