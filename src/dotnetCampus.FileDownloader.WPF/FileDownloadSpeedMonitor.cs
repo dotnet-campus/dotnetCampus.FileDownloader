b6dc59f787fb8fa091d4d8a79603fec5c592961d
lindexi <lindexi_gd@163.com>
2020-07-22 08:36:23 +0800

    支持设置下载监控时间

--- src/dotnetCampus.FileDownloader.WPF/FileDownloadSpeedMonitor.cs
+++ src/dotnetCampus.FileDownloader.WPF/FileDownloadSpeedMonitor.cs
@@ -4,8 +4,23 @@ using dotnetCampus.FileDownloader.Tool;
 
 namespace dotnetCampus.FileDownloader.WPF
 {
+    /// <summary>
+    /// 文件下载速度监控
+    /// </summary>
     public class FileDownloadSpeedMonitor : IDisposable
     {
+        /// <summary>
+        /// 文件下载速度监控
+        /// </summary>
+        /// <param name="delayTime">触发事件延迟时间</param>
+        public FileDownloadSpeedMonitor(TimeSpan? delayTime)
+        {
+            if (delayTime is null)
+            {
+                DelayTime = TimeSpan.FromMilliseconds(500);
+            }
+        }
+
         public void Start()
         {
             _started = true;
@@ -14,7 +29,7 @@ namespace dotnetCampus.FileDownloader.WPF
             {
                 while (_started)
                 {
-                    await Task.Delay(TimeSpan.FromMilliseconds(500));
+                    await Task.Delay(DelayTime);
 
                     if (!_started)
                     {
@@ -27,7 +42,8 @@ namespace dotnetCampus.FileDownloader.WPF
                     }
 
                     var speed = GetCurrentSpeed();
-                    var downloadInfoProgress = new DownloadInfoProgress(FileSizeFormatter.FormatSize(_currentDownloadProgress.FileLength),
+                    var downloadInfoProgress = new DownloadInfoProgress(
+                        FileSizeFormatter.FormatSize(_currentDownloadProgress.FileLength),
                         $"{FileSizeFormatter.FormatSize(_currentDownloadProgress.DownloadedLength)}/{FileSizeFormatter.FormatSize(_currentDownloadProgress.FileLength)}",
                         speed, _currentDownloadProgress);
 
@@ -51,13 +67,16 @@ namespace dotnetCampus.FileDownloader.WPF
         {
             var downloadedLength = _currentDownloadProgress!.DownloadedLength - _lastDownloadLength!.Value;
 
-            var text = ($"{ FileSizeFormatter.FormatSize(downloadedLength * 1000.0 / (DateTime.Now - _lastDateTime).TotalMilliseconds)}/s");
+            var text =
+                ($"{FileSizeFormatter.FormatSize(downloadedLength * 1000.0 / (DateTime.Now - _lastDateTime).TotalMilliseconds)}/s"
+                );
 
             _lastDateTime = DateTime.Now;
 
             return text;
         }
 
+        private TimeSpan DelayTime { get; }
 
         private DateTime _lastDateTime = DateTime.Now;
 
