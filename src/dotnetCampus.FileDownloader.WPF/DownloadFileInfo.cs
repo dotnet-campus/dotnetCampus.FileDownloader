68fcc5f3e4735c5d1d538e669a19f439b17d3b41
lindexi <lindexi_gd@163.com>
2020-08-04 14:56:33 +0800

    优化属性的通知

--- src/dotnetCampus.FileDownloader.WPF/DownloadFileInfo.cs
+++ src/dotnetCampus.FileDownloader.WPF/DownloadFileInfo.cs
@@ -8,11 +8,16 @@ namespace dotnetCampus.FileDownloader.WPF
 {
     public class DownloadFileInfo : INotifyPropertyChanged
     {
-        private string _downloadProcess;
-        private string _fileSize;
-        private string _downloadSpeed;
+        /// <summary>
+        ///     文件名
+        /// </summary>
+        /// 文件名不可变，因此不带通知
         public string FileName { get; set; }
 
+        /// <summary>
+        ///     文件的大小
+        /// </summary>
+        /// 也许服务器骗我，因此文件的大小也许会更改
         public string FileSize
         {
             get => _fileSize;
@@ -24,6 +29,9 @@ namespace dotnetCampus.FileDownloader.WPF
             }
         }
 
+        /// <summary>
+        ///     下载进度，用于绑定
+        /// </summary>
         public string DownloadProcess
         {
             get => _downloadProcess;
@@ -35,14 +43,41 @@ namespace dotnetCampus.FileDownloader.WPF
             }
         }
 
+        /// <summary>
+        ///     下载项被加入的时间
+        /// </summary>
+        /// 加入时间不可变，因此不带通知
         public string AddedTime { get; set; }
 
+        /// <summary>
+        ///     下载链接
+        /// </summary>
+        /// 下载链接不可变，因此不带通知
         public string DownloadUrl { get; set; }
 
+        /// <summary>
+        ///     下载的本地文件保存路径
+        /// </summary>
+        /// 下载的本地文件保存路径不可变，因此不带通知
         public string FilePath { get; set; }
 
-        public bool IsFinished { get; set; }
+        /// <summary>
+        ///     是否下载完成
+        /// </summary>
+        public bool IsFinished
+        {
+            get => _isFinished;
+            set
+            {
+                if (value == _isFinished) return;
+                _isFinished = value;
+                OnPropertyChanged();
+            }
+        }
 
+        /// <summary>
+        ///     当前下载的速度
+        /// </summary>
         [JsonIgnore]
         public string DownloadSpeed
         {
@@ -62,16 +97,17 @@ namespace dotnetCampus.FileDownloader.WPF
         {
             var dispatcher = Application.Current.Dispatcher;
             if (dispatcher.CheckAccess())
-            {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
             else
-            {
                 dispatcher.InvokeAsync(() =>
                 {
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
                 });
-            }
         }
+
+        private string _downloadProcess;
+        private string _downloadSpeed;
+        private string _fileSize;
+        private bool _isFinished;
     }
 }
