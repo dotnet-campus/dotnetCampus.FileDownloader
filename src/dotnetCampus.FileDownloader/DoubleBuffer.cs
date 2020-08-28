c024119f50c91da6541bd1929e097d2d46daf5f7
lindexi <lindexi_gd@163.com>
2020-08-28 21:12:31 +0800

    修复构建

--- src/dotnetCampus.FileDownloader/DoubleBuffer.cs
+++ src/dotnetCampus.FileDownloader/DoubleBuffer.cs
@@ -8,17 +8,6 @@ namespace dotnetCampus.FileDownloader
     /// 提供双缓存 线程安全列表
     /// </summary>
     /// 写入的时候写入到一个列表，通过 SwitchBuffer 方法，可以切换当前缓存
-    class DoubleBuffer<T> : DoubleBuffer<List<T>, T>
-    {
-        public DoubleBuffer() : base(new List<T>(), new List<T>())
-        {
-        }
-    }
-
-    /// <summary>
-    /// 提供双缓存 线程安全列表
-    /// </summary>
-    /// 写入的时候写入到一个列表，通过 SwitchBuffer 方法，可以切换当前缓存
     class DoubleBuffer<T, TU> where T : class, ICollection<TU>
     {
         public DoubleBuffer(T aList, T bList)
---
+++ src/dotnetCampus.FileDownloader/DoubleBuffer`T.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace dotnetCampus.FileDownloader
+{
+    /// <summary>
+    /// 提供双缓存 线程安全列表
+    /// </summary>
+    /// 写入的时候写入到一个列表，通过 SwitchBuffer 方法，可以切换当前缓存
+    class DoubleBuffer<T> : DoubleBuffer<List<T>, T>
+    {
+        public DoubleBuffer() : base(new List<T>(), new List<T>())
+        {
+        }
+    }
+}
