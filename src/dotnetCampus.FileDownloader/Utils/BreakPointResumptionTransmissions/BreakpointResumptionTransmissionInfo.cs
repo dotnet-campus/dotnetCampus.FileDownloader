b8117eb8d8b76cd2d43fb5075530a40075241791
lindexi <lindexi_gd@163.com>
2022-07-16 16:30:40 +0800

    定义断点续传信息

---
+++ src/dotnetCampus.FileDownloader/Utils/BreakPointResumptionTransmissions/BreakpointResumptionTransmissionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace dotnetCampus.FileDownloader.Utils.BreakPointResumptionTransmissionManager;
+
+/// <summary>
+/// 断点续传信息
+/// </summary>
+class BreakpointResumptionTransmissionInfo
+{
+    public BreakpointResumptionTransmissionInfo(long downloadLength, List<(long startPoint, long length)>? downloadedInfo = null)
+    {
+        DownloadLength = downloadLength;
+        DownloadedInfo = downloadedInfo;
+    }
+
+    /// <summary>
+    /// 下载的长度
+    /// </summary>
+    public long DownloadLength { get; }
+
+    /// <summary>
+    /// 已经下载的信息
+    /// </summary>
+    public List<(long startPoint, long length)>? DownloadedInfo { get; }
+}
