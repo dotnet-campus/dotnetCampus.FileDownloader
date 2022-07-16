bf3fe58ac6585f12a9e9fae5ec8178f4adc79a85
lindexi <lindexi_gd@163.com>
2022-07-16 11:11:06 +0800

    Revert "删除断点续传信息"
    This reverts commit f7c7c8f336703b6fb3843fe14f92f8f20e9befec.

--- src/dotnetCampus.FileDownloader/DownloadByHttpClient/SegmentFileDownloaderByHttpClient.cs
+++ src/dotnetCampus.FileDownloader/DownloadByHttpClient/SegmentFileDownloaderByHttpClient.cs
@@ -32,11 +32,12 @@ public class SegmentFileDownloaderByHttpClient : IDisposable
     /// <param name="sharedArrayPool">共享缓存数组池，默认使用 ArrayPool 池</param>
     /// <param name="bufferLength">缓存的数组长度，默认是 65535 的长度</param>
     /// <param name="stepTimeOut">每一步 每一分段下载超时时间 默认 10 秒</param>
+    /// <param name="breakpointResumptionTransmissionRecordFile">断点续下的信息记录文件，如为空将不带上断点续下功能</param>
     public SegmentFileDownloaderByHttpClient(string url, FileInfo file,
         HttpClient? httpClient = null,
         ILogger<SegmentFileDownloader>? logger = null,
             IProgress<DownloadProgress>? progress = null, ISharedArrayPool? sharedArrayPool = null,
-            int bufferLength = ushort.MaxValue, TimeSpan? stepTimeOut = null)
+            int bufferLength = ushort.MaxValue, TimeSpan? stepTimeOut = null, FileInfo? breakpointResumptionTransmissionRecordFile = null)
     {
         _logger = logger ?? new DebugSegmentFileDownloaderLogger();
         _progress = progress ?? new Progress<DownloadProgress>();
