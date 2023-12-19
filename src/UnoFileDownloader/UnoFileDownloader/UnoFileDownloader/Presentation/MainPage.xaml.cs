b033d24170d24b2d6ef491bdbd75139ba05b1c74
lindexi <lindexi_gd@163.com>
2023-12-19 12:06:39 +0800

    初始化项目

---
+++ src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UnoFileDownloader.Presentation
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/NewTaskModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoFileDownloader.Presentation
+{
+    public partial record NewTaskModel
+    {
+    }
+}
