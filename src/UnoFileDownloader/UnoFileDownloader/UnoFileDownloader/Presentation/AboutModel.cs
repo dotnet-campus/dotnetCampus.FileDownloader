b2e22686c029d00c30f74f617759b7182faf0f92
lindexi <lindexi_gd@163.com>
2023-12-20 20:29:58 +0800

    测试后台线程更改

--- src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/AboutModel.cs
+++ src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/AboutModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,25 @@ using UnoFileDownloader.Utils;
 
 namespace UnoFileDownloader.Presentation
 {
-    public partial record AboutModel(INavigator Navigator, IDispatcherQueueProvider DispatcherQueueProvider)
+    public partial record AboutModel(INavigator Navigator, IDispatcherQueueProvider DispatcherQueueProvider) : INotifyPropertyChanged
     {
+        private string _appInfo = string.Empty;
+
+        public string AppInfo
+        {
+            set
+            {
+                if (value == _appInfo)
+                {
+                    return;
+                }
+
+                _appInfo = value;
+                OnPropertyChanged();
+            }
+            get => _appInfo;
+        }
+
         public async Task CloseAbout()
         {
             var response = await Navigator.NavigateBackAsync(this);
@@ -27,8 +46,31 @@ namespace UnoFileDownloader.Presentation
             {
                 _ = Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/dotnet-campus/dotnetCampus.FileDownloader"));
             });
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    AppInfo = Path.GetRandomFileName();
+                    await Task.Delay(100);
+                }
+            });
         }
 
         protected DispatcherQueue Dispatcher { get; } = DispatcherQueueProvider.Dispatcher;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
