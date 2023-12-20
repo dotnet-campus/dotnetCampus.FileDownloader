1bf1065c827522e3659382b74eacb8974b057021
lindexi <lindexi_gd@163.com>
2023-12-20 19:31:06 +0800

    绑定下载列表

--- src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/MainModel.cs
+++ src/UnoFileDownloader/UnoFileDownloader/UnoFileDownloader/Presentation/MainModel.cs
@@ -1,21 +1,36 @@
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
+
+using Uno.Extensions;
+
+using UnoFileDownloader.Business;
+using UnoFileDownloader.Utils;
+
 namespace UnoFileDownloader.Presentation
 {
     public partial record MainModel
     {
         private INavigator _navigator;
+        private readonly DownloadFileListManager _downloadFileListManager;
 
         public MainModel(
             IStringLocalizer localizer,
             IOptions<AppConfig> appInfo,
-            INavigator navigator)
+            INavigator navigator, DownloadFileListManager downloadFileListManager)
         {
             _navigator = navigator;
+            _downloadFileListManager = downloadFileListManager;
             Title = localizer["Main"];
             Title += $" - {localizer["ApplicationName"]}";
             Title += $" - {appInfo?.Value?.Environment}";
+
+            UpdateDownloadFileInfoViewList();
         }
 
         public string? Title { get; }
+
+        public ObservableCollection<DownloadFileInfo> DownloadFileInfoViewList { get; } =
+            new ObservableCollection<DownloadFileInfo>();
 
         public IState<string> Name => State<string>.Value(this, () => string.Empty);
 
@@ -33,6 +48,12 @@ namespace UnoFileDownloader.Presentation
         public async Task GoToAbout()
         {
             await _navigator.NavigateViewModelAsync<AboutModel>(this);
-        }   
+        }
+
+        private void UpdateDownloadFileInfoViewList()
+        {
+            DownloadFileInfoViewList.Clear();
+            DownloadFileInfoViewList.AddRange(_downloadFileListManager.DownloadFileInfoList);
+        }
     }
 }
