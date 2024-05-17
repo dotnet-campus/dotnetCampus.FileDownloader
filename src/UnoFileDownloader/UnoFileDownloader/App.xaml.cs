c722f5e9a7559cf50a30715cd7c0f0b2876e948c
lindexi <lindexi_gd@163.com>
2024-05-17 20:44:32 +0800

    重新配置跳转逻辑

--- src/UnoFileDownloader/UnoFileDownloader/App.xaml.cs
+++ src/UnoFileDownloader/UnoFileDownloader/App.xaml.cs
@@ -1,8 +1,14 @@
 using System;
 
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 
 using Uno.Resizetizer;
+using UnoFileDownloader.Business;
+using UnoFileDownloader.Business.Models;
+using UnoFileDownloader.Presentation;
+using UnoFileDownloader.Utils;
 
 namespace UnoFileDownloader;
 public partial class App : Application
@@ -17,39 +23,95 @@ public partial class App : Application
     }
 
     protected Window? MainWindow { get; private set; }
+    protected IHost? Host { get; private set; }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected async override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        MainWindow = new Window();
+       
+        
+        var builder = this.CreateBuilder(args)
+            .UseToolkitNavigation()
+            .Configure(host =>
+            {
+                host.UseConfiguration(configure: configBuilder =>
+                        configBuilder
+                            .EmbeddedSource<App>()
+                            .Section<AppConfig>())
+                    .UseLocalization()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddSingleton<IDispatcherQueueProvider>(c =>
+                        {
+                            return new DispatcherQueueProvider(MainWindow!.DispatcherQueue);
+                        });
+                        
+                        // 下载文件管理器
+                        services.AddSingleton<DownloadFileListManager>();
+                    })
+                    .UseNavigation(ReactiveViewModelMappings.ViewModelMappings, RegisterRoutes)
+                    ;
+            });
+        MainWindow = builder.Window;
+        //MainWindow = new Window();
 #if DEBUG
         MainWindow.EnableHotReload();
 #endif
+        Host = await builder.NavigateAsync<Shell>();
+        var localizer = (Uno.Extensions.Localization.ResourceLoaderStringLocalizer) Host.Services.GetRequiredService<IStringLocalizer>();
+        string title = localizer["ApplicationName"];
+        
+        MainWindow.Title = title;
 
+        //// Do not repeat app initialization when the Window already has content,
+        //// just ensure that the window is active
+        //if (MainWindow.Content is not Frame rootFrame)
+        //{
+        //    // Create a Frame to act as the navigation context and navigate to the first page
+        //    rootFrame = new Frame();
 
-        // Do not repeat app initialization when the Window already has content,
-        // just ensure that the window is active
-        if (MainWindow.Content is not Frame rootFrame)
-        {
-            // Create a Frame to act as the navigation context and navigate to the first page
-            rootFrame = new Frame();
+        //    // Place the frame in the current Window
+        //    MainWindow.Content = rootFrame;
 
-            // Place the frame in the current Window
-            MainWindow.Content = rootFrame;
+        //    rootFrame.NavigationFailed += OnNavigationFailed;
+        //}
 
-            rootFrame.NavigationFailed += OnNavigationFailed;
-        }
-
-        if (rootFrame.Content == null)
-        {
-            // When the navigation stack isn't restored navigate to the first page,
-            // configuring the new page by passing required information as a navigation
-            // parameter
-            rootFrame.Navigate(typeof(MainPage), args.Arguments);
-        }
+        //if (rootFrame.Content == null)
+        //{
+        //    // When the navigation stack isn't restored navigate to the first page,
+        //    // configuring the new page by passing required information as a navigation
+        //    // parameter
+        //    rootFrame.Navigate(typeof(Shell), args.Arguments);
+        //}
 
         MainWindow.SetWindowIcon();
         // Ensure the current window is active
         MainWindow.Activate();
+    }
+    
+    private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
+    {
+        views.Register
+        (
+            new ViewMap(ViewModel: typeof(ShellModel), View: typeof(Shell)),
+            new ViewMap<MainPage, MainModel>(),
+            new ViewMap<AboutPage, AboutModel>(),
+            new ViewMap<NewTaskPage, NewTaskModel>(),
+            new DataViewMap<SecondPage, SecondModel, Entity>()
+        );
+        
+        // 必须写注册哦，否则无法跳转
+        routes.Register
+        (
+            new RouteMap("", View: views.FindByViewModel<ShellModel>(),
+                Nested: new RouteMap[]
+                {
+                    new RouteMap("Main",   View: views.FindByViewModel<MainModel>()),
+                    new RouteMap("Second", View: views.FindByViewModel<SecondModel>()),
+                    new RouteMap("About",  View: views.FindByViewModel<AboutModel>()),
+                    new RouteMap("NewTask",View: views.FindByViewModel<NewTaskModel>()),
+                }
+            )
+        );
     }
 
     /// <summary>
