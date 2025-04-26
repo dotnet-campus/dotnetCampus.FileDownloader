adae6998b4cf414605cb524d3ad9bc68a9e53608
lindexi <lindexi_gd@163.com>
2025-04-26 17:01:50 +0800

    去掉傻逼代码

--- src/dotnetCampus.FileDownloader/Utils/BreakpointResumptionTransmissions/DataRange.cs
+++ src/dotnetCampus.FileDownloader/Utils/BreakpointResumptionTransmissions/DataRange.cs
@@ -19,20 +19,21 @@ readonly struct DataRange : IComparer<DataRange>, IEquatable<DataRange>
 
     public int Compare(DataRange x, DataRange y)
     {
-        if (ReferenceEquals(x, y))
-        {
-            return 0;
-        }
+        // 由于 DataRange 从引用类型修改为值类型，这就导致原本调用 ReferenceEquals 的代码为傻逼代码，注释掉，避免无用的装箱判断不相等
+        //if (ReferenceEquals(x, y))
+        //{
+        //    return 0;
+        //}
 
-        if (ReferenceEquals(null, y))
-        {
-            return 1;
-        }
+        //if (ReferenceEquals(null, y))
+        //{
+        //    return 1;
+        //}
 
-        if (ReferenceEquals(null, x))
-        {
-            return -1;
-        }
+        //if (ReferenceEquals(null, x))
+        //{
+        //    return -1;
+        //}
 
         return x.StartPoint.CompareTo(y.StartPoint);
     }
@@ -66,15 +67,16 @@ readonly struct DataRange : IComparer<DataRange>, IEquatable<DataRange>
 
     public bool Equals(DataRange other)
     {
-        if (ReferenceEquals(null, other))
-        {
-            return false;
-        }
+        // 由于 DataRange 从引用类型修改为值类型，这就导致原本调用 ReferenceEquals 的代码为傻逼代码，注释掉，避免无用的装箱判断不相等
+        //if (ReferenceEquals(null, other))
+        //{
+        //    return false;
+        //}
 
-        if (ReferenceEquals(this, other))
-        {
-            return true;
-        }
+        //if (ReferenceEquals(this, other))
+        //{
+        //    return true;
+        //}
 
         return StartPoint == other.StartPoint && Length == other.Length;
     }
@@ -84,11 +86,6 @@ readonly struct DataRange : IComparer<DataRange>, IEquatable<DataRange>
         if (ReferenceEquals(null, obj))
         {
             return false;
-        }
-
-        if (ReferenceEquals(this, obj))
-        {
-            return true;
         }
 
         if (obj.GetType() != GetType())
@@ -103,7 +100,11 @@ readonly struct DataRange : IComparer<DataRange>, IEquatable<DataRange>
     {
         unchecked
         {
+#if NETCOREAPP3_1_OR_GREATER
+            return HashCode.Combine(StartPoint, Length);
+#else
             return (StartPoint.GetHashCode() * 397) ^ Length.GetHashCode();
+#endif
         }
     }
 
